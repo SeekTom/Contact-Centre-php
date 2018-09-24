ca23a471f2b47611c2b4096d44504564721c274b
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-09-24 14:51:48 +0100

    Update enqueue_call.php
    removed workflow array.

--- views/enqueue_call.php
+++ views/enqueue_call.php
@@ -4,6 +4,7 @@ header('Content-type: application/xml');
 
 require 'vendor/autoload.php';
 use Twilio\Twiml;
+$WorkflowSid ="";
 
 $department = [
 1 => "sales",
@@ -11,17 +12,11 @@ $department = [
 3 => "marketing"
 ];
 
-$workflow_array = [
-1 => getenv("TWILIO_ACME_SALES_WORKFLOW_SID"),
-2 => getenv("TWILIO_ACME_SUPPORT_WORKFLOW_SID"),
-3 => getenv("TWILIO_ACME_MARKETING_WORKFLOW_SID")
-];
-
 $response = new  Twiml();
 
 $choice = $_REQUEST['Digits'];
 
-$enqueue = $response->enqueue([workflowSid=>$workflow_array[$choice]]);
+$enqueue = $response->enqueue([workflowSid=>$WorkflowSid]);
 	$enqueue->task('{"selected_product":"'.$department[$choice].'"}');
 
 echo $response;
